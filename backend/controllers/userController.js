8a49ea61bcc0a031600479e16d3db4ab5207681c
Varium-Coding <C_tout@live.co.uk>
2024-06-05 14:39:58 +0100

    frontendaddon

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,8 @@ const User = require('../models/User');
 
 exports.getUserProfile = async (req, res) => {
     try {
-        const user = await User.findById(req.user.id).select('-password');
+        const userId = req.params.id;
+        const user = await User.findById(userId).select('-password');
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
