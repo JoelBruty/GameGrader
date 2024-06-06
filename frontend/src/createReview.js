266071596f12b95566f0b5a25b8e8822532cbf9c
Varium-Coding <C_tout@live.co.uk>
2024-06-06 10:14:20 +0100

    fixed2electricboogaloo

--- frontend/src/createReview.js
+++ frontend/src/createReview.js
@@ -1,31 +1,32 @@
-import React, {useState} from 'react'
-import axios from 'axios'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function createReview({userId}) {
-    const [formData, setFormData] = useState ({gameId: '', reviewText:'', rating: ''})
+function CreateReview({ userId }) {
+  const [formData, setFormData] = useState({ gameId: '', reviewText: '', rating: '' });
 
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value})
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:4000/review/create', { ...formData, userId });
+      alert('Review created successfully');
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-            const respone = await axios.post('http://localhost:4000/review/create')
-            alert('Review created successfully')
-        } catch (error) {
-            console.error(error)
-        }
-    }
-    return (
-        <form onSubmit={handleSubmit}>
-            <h2>Create Review</h2>
-            <input type="text" name="gameId" placeholder="Game ID" onChange={handleChange} required />
-            <textarea type="reviewText" placeholder="Review" onChange={handleChange} required />
-            <input type="number" name="rating" placeholder="Rating" onChange={handleChange} required />
-            <button type="Submit">Submit Review</button>
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Create Review</h2>
+      <input type="text" name="gameId" placeholder="Game ID" onChange={handleChange} required />
+      <textarea name="reviewText" placeholder="Review" onChange={handleChange} required></textarea>
+      <input type="number" name="rating" placeholder="Rating" onChange={handleChange} required />
+      <button type="submit">Submit Review</button>
+    </form>
+  );
 }
 
-export default createReview
+export default CreateReview;
