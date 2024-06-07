75ea51638392751cec8ac72a13732087c7e52317
JoelBruty <165796967+JoelBruty@users.noreply.github.com>
2024-06-07 15:54:28 +0100

    getGameDetails

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 require('dotenv').config();
 
-exports.twitchAuth = async (req, res) => {
+exports.twitchAuth = async (req, res) => { //POST http://localhost:4000/game/twitchauth
     try {
         const response = await axios.post(
             'https://id.twitch.tv/oauth2/token',
@@ -18,7 +18,7 @@ exports.twitchAuth = async (req, res) => {
     }
 };
 
-exports.searchGames = async (req, res) => {
+exports.searchGames = async (req, res) => { //GET http://localhost:4000/game/search?query=Mario
     const { query } = req.query;
 
     try {
@@ -39,3 +39,23 @@ exports.searchGames = async (req, res) => {
     }
 };
 
+exports.getGameDetails = async (req, res) => { //GET http://localhost:4000/game/details?id=84920
+    const { id } = req.query;
+
+    try {
+        const response = await axios.post(
+            'https://api.igdb.com/v4/games',
+            `fields id, name, collection.name, genres.name, cover.image_id, involved_companies.company.name, platforms.name, release_dates.human, release_dates.platform.name, release_dates.region, summary, websites.url, websites.trusted, similar_games.name; where id = ${id};`,
+            {
+                headers: {
+                    'Client-ID': process.env.TWITCH_CLIENT_ID,
+                    'Authorization': `Bearer ${process.env.TWITCH_ACCESS_TOKEN}`,
+                },
+            }
+        );
+
+        res.json(response.data);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
--- backend/routes/game.js
+++ backend/routes/game.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const gameController = require('../controllers/gameController')
 
 router.get('/search', gameController.searchGames)
+router.get('/details', gameController.getGameDetails)
 router.post('/twitchauth', gameController.twitchAuth)
 module.exports = router
