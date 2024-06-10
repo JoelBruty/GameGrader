7b4478ba360df848c9968916e7b23bfc1715c5b1
Varium-Coding <C_tout@live.co.uk>
2024-06-10 09:52:01 +0100

    +gc change + mw

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -1,24 +1,40 @@
 const axios = require('axios');
+const Game = require('../models/Game');
 require('dotenv').config();
 
 exports.searchGames = async (req, res) => {
-    const { query } = req.query;
+  const { query } = req.query;
 
-    try {
-        const response = await axios.post(
-            'https://api.igdb.com/v4/games',
-            `search "${query}"; fields name, cover.url;`,
-            {
-                headers: {
-                    'Client-ID': process.env.TWITCH_CLIENT_ID,
-                    'Authorization': `Bearer ${process.env.TWITCH_ACCESS_TOKEN}`,
-                },
-            }
-        );
+  try {
+    let games = await Game.find({ name: new RegExp(query, 'i') });
 
-        res.json(response.data);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+    if (games.length === 0) {
+      const response = await axios.post(
+        'https://api.igdb.com/v4/games',
+        `search "${query}"; fields name, cover.url;`,
+        {
+          headers: {
+            'Client-ID': process.env.TWITCH_CLIENT_ID,
+            'Authorization': `Bearer ${process.env.TWITCH_ACCESS_TOKEN}`,
+          },
+        }
+      );
+
+      games = response.data.map(async gameData => {
+        const newGame = new Game({
+          igdbId: gameData.id,
+          name: gameData.name,
+          coverUrl: gameData.cover?.url,
+        });
+        await newGame.save();
+        return newGame;
+      });
+      games = await Promise.all(games);
     }
+
+    res.json(games);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
